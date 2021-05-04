6e80b96eab96a0f2e2df09f081f88a5fc5703ce2
JB519 <54725293+JB519@users.noreply.github.com>
2021-05-04 09:43:08 -0400

    Add files via upload
    For curling

---
+++ installation.sh
@@ -0,0 +1,5 @@
+apt-get install postgresql
+apt-get install postgresql-client
+sudo service postgresql start
+sudo apt install nodejs
+sudo npm install pm2 -g
---
+++ serversetup.sh
@@ -0,0 +1,18 @@
+git clone -b demo https://github.com/CPSC430AlumniDB/AlumniDB.git
+cd AlumniDB
+cd alumni-search
+npm install
+cd frontend
+npm install
+cd pages
+sed -i 's/localhost/34.123.130.56/' *
+cd ../lib
+sed -i 's/localhost/34.123.130.56/' *
+cd ..
+cd ..
+sed -i 's/INSERT INTO admin//' *.sql
+psql -f alumnidb.sql
+pm2 start server.js
+cd frontend
+pm2 start 'npm run dev'
+
